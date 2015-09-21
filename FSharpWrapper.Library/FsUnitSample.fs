4f52aca24d6029b218e5dd519e2a27ca3cac2355
Daniel Rozo <daniel@danielrozo.es>
2015-09-21 23:39:56 +0200

    Added more tests

--- FSharpWrapper.Library/FsUnitSample.fs
+++ FSharpWrapper.Library/FsUnitSample.fs
@@ -18,12 +18,14 @@ type ``Word Wrapper Should`` ()=
            wrapper.Wrap("abc", 1) |> should equal "a\nb\nc"
 
     [<Test>] member test.
-    ``wrap when the column size is 2`` ()=
-           wrapper.Wrap("abc", 2) |> should equal "ab\nc"
-
-    [<Test>] member test.
     ``wrap when the column size is 2 with 5 characters`` ()=
            wrapper.Wrap("abcde", 2) |> should equal "ab\ncd\ne"
 
+    [<Test>] member test.
+    ``wrap when columnWidth is different than 2`` ()=
+           wrapper.Wrap("babc", 3) |> should equal "bab\nc"
 
+    [<Test>] member test.
+    ``wrap when there are spaces`` ()=
+           wrapper.Wrap("b b b b", 4) |> should equal "b b \nb b"
 
--- FSharpWrapper.Library/Library1.fs
+++ FSharpWrapper.Library/Library1.fs
@@ -3,7 +3,7 @@ open System
 
 type Wrapper() = 
     member this.Separator(index, colWidth, length) =
-         if (index - 1) % colWidth = 0 && index < length - 1 then "\n" else ""
+         if (index + 1) % colWidth = 0 && index < length - 1 then "\n" else ""
 
     member this.Wrap(input : string, columnWidth) =
         input.ToCharArray() 
